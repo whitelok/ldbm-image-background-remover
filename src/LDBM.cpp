1c3cb5b5df2b474b3d958c3360e46b84520a3bb9
Sebastian Martin Dicke <sebastianmartindicke@gmx.de>
2021-09-20 16:14:33 +0200

    Fix misspelled include filename

--- src/LDBM.cpp
+++ src/LDBM.cpp
@@ -5,7 +5,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <eigen3/Eigen/core>
+#include <eigen3/Eigen/Core>
 #include <eigen3/Eigen/Dense>
 #include <eigen3/Eigen/Sparse>
 #include <eigen3/Eigen/SparseQR>
